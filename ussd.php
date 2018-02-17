5d68220285dbd98112aedeecd380cc4ef484e8c9
waploaj <waploaj@icloud.com>
2018-02-17 22:51:29 +0300

    initial commit

---
+++ ussd.php
@@ -0,0 +1,7 @@
+<!-- author = waploaj -->
+#!/usr/bin/php
+<?php
+define("CR", "\r");
+define("TERMINAL_DEVICE", "/dev/ttyUSB0");
+define("ADDITIONAL_DEVICE", "/dev/ttyUSB2");
+$args = $_SERVER['argc'];
