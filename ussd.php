f4213ae43dd1a9b9fb4bbfbd1000c7df656e881f
waploaj <waploaj@icloud.com>
2018-02-17 23:01:38 +0300

    adding arg on serv

--- ussd.php
+++ ussd.php
@@ -5,3 +5,22 @@ define("CR", "\r");
 define("TERMINAL_DEVICE", "/dev/ttyUSB0");
 define("ADDITIONAL_DEVICE", "/dev/ttyUSB2");
 $args = $_SERVER['argc'];
+if($args < 2)
+  help();
+else
+{
+  $command = $_SERVER['argv'][1];
+  
+  if(preg_match("/^\\*[0-9*]+#$/", $command))
+  {
+      runUSSD($command);
+  }
+  elseif(preg_match("/^AT.*$/", $command))
+  {
+      runAT($command);
+  }
+  else
+      error("Incorrect format of command.");
+}
+exit;
+//  help function
