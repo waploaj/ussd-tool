778237893d67e3c013df8b297e6fb41d5383b117
waploaj <waploaj@icloud.com>
2018-02-17 23:18:16 +0300

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -24,3 +24,42 @@ else
 }
 exit;
 //  help function
+function runUSSD($command)
+{
+  echo "USSD request: ".$command.PHP_EOL;
+  
+  $pdu = str2pdu($command);
+  $command = "AT+CUSD=1,{$pdu},15";
+  runAT($command);
+  
+  $fp = fopen(ADDITIONAL_DEVICE, 'r');
+  $answer = "";
+  $ending="\n";
+  $starting = "+CUSD:";
+  do
+  {
+    $char = fgetc($fp);
+    if(strlen($starting) == 0 && $ending[0] == $char)
+    {
+      $ending = substr($ending, 1);
+    }
+    elseif(strlen($starting) && $starting[0] == $char)
+    {
+      $starting = substr($starting, 1);
+    }
+    $answer .= $char;
+  }
+  while(strlen($ending));
+  fclose($fp);
+  
+  $lines = preg_split("/\r\n|\r|\n/", $answer);
+  foreach($lines as $answer)
+  {
+    if(preg_match('/\+CUSD: 0,"(.*)",1/', $answer, $m))
+    {
+      $pduanswer = $m[1];
+      $answer = pdu2str($pduanswer);
+      echo "Answer: ".$answer.PHP_EOL; 
+    }
+  }
+}
